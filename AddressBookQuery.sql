760bb6285d30eb2a9115a09ee5d3b1957b2be548
varsha Gunjal <varshashingote01@gmail.com>
2023-03-04 15:09:43 +0530

    [varsha]Recored inserted

--- AddressBookQuery.sql
+++ AddressBookQuery.sql
@@ -10,3 +10,14 @@ create Database Address_Book_Service;
 create Table Address_Book
 (First_Name varchar(100), Last_Name varchar(100), Address varchar(300), 
 City varchar(50), State varchar(30), Zip_Code int, Phone_Number bigint, E_Mail varchar(50) )
+
+----------UC3----------------
+-----------insert Record into Table-------
+insert into Address_Book values
+('Varsha', 'Gunjal', 'primprichinchwad', 'Pune', 'Maharastra', 422324, 8788583058, 'varsha@gmail.com'),
+('Suraj', 'shingote', 'Shirdi', 'ANGAR','Maharasta', 234567, 5678922, 'suraj@gmail.com'),
+('vasu', 'shingote', 'kalwwadiphata', 'pune', 'MH', 127324, 123456789, 'vasu@gmail.com'),
+('Chaya', 'Gunjal', 'Sangamner', 'Anagr', 'MH', 422605, 41234567, 'chaya@gmail.com'),
+('bhima', 'Gunjal', 'Sangamner', 'Anager', 'MH', 211011, 5555555555, 'Bhima@gmail.com'),
+('Aditya', 'Gunjal', 'Sangamer', 'Anagar', 'MH', 23456789, 62345678, 'Aditya@gmail.com')
+
