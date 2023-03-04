05ed94ebcc532ebc6bcaf6d65d0ace6fd8f2839f
varsha Gunjal <varshashingote01@gmail.com>
2023-03-04 14:40:01 +0530

    [varsha] Table is Created

--- AddressBookQuery.sql
+++ AddressBookQuery.sql
@@ -1,1 +1,12 @@
+-----UC1---------------------
+-----------createtable---------
+
 create Database Address_Book_Service;
+
+
+-----------Uc2----------------
+-----------Create Table-------
+
+create Table Address_Book
+(First_Name varchar(100), Last_Name varchar(100), Address varchar(300), 
+City varchar(50), State varchar(30), Zip_Code int, Phone_Number bigint, E_Mail varchar(50) )
